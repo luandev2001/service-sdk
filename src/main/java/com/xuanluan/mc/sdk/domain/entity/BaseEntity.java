96d735a870a2337b56c73c0b2c7b6db8b0069d48
xluan1 <shortshort1m6@gmail.com>
2023-12-13 00:28:35 +0700

    hot fix

--- src/main/java/com/xuanluan/mc/sdk/domain/entity/BaseEntity.java
+++ src/main/java/com/xuanluan/mc/sdk/domain/entity/BaseEntity.java
@@ -1,19 +1,31 @@
 package com.xuanluan.mc.sdk.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.xuanluan.mc.sdk.utils.StringUtils;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
+import java.util.Date;
 
 @Getter
 @Setter
 @MappedSuperclass
-public class BaseEntity extends EntityModel {
+public class BaseEntity {
     @Id
     private String id;
+    @Column(updatable = false)
+    private String createdBy;
+    private String updatedBy;
+    @Column(nullable = false, updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    private Date createdAt = new Date();
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    private Date updatedAt;
+    private boolean isActive = true;
 
     @PrePersist
     private void setDefault() {
