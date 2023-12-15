3982e66dd89c1d1320cdada715228b18e6196f93
xluan1 <shortshort1m6@gmail.com>
2023-12-15 22:10:05 +0700

    disable bean

--- src/main/java/com/xuanluan/mc/sdk/service/tenant/TenantIdentifierResolver.java
+++ src/main/java/com/xuanluan/mc/sdk/service/tenant/TenantIdentifierResolver.java
@@ -3,9 +3,7 @@ package com.xuanluan.mc.sdk.service.tenant;
 import com.xuanluan.mc.sdk.service.constant.BaseConstant;
 import com.xuanluan.mc.sdk.utils.StringUtils;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
-import org.springframework.stereotype.Component;
 
-@Component
 public class TenantIdentifierResolver implements CurrentTenantIdentifierResolver {
     @Override
     public String resolveCurrentTenantIdentifier() {
