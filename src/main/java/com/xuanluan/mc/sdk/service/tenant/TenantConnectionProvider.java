792efaadf00e96ef81739fada697e0c7c0b63e38
xluan1 <shortshort1m6@gmail.com>
2023-12-17 20:40:26 +0700

    add hibernate property multi tenant

--- src/main/java/com/xuanluan/mc/sdk/service/tenant/TenantConnectionProvider.java
+++ src/main/java/com/xuanluan/mc/sdk/service/tenant/TenantConnectionProvider.java
@@ -3,14 +3,17 @@ package com.xuanluan.mc.sdk.service.tenant;
 import com.xuanluan.mc.sdk.service.constant.BaseConstant;
 import com.xuanluan.mc.sdk.utils.AssertUtils;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.engine.jdbc.connections.spi.AbstractDataSourceBasedMultiTenantConnectionProviderImpl;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Map;
 
 @RequiredArgsConstructor
-public class TenantConnectionProvider extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl {
+public class TenantConnectionProvider extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl implements HibernatePropertiesCustomizer {
     private final DataSource dataSource;
 
     @Override
@@ -27,5 +30,16 @@ public class TenantConnectionProvider extends AbstractDataSourceBasedMultiTenant
             AssertUtils.isTrue(false, "error.invalid_tenant", "");
         }
         return dataSource;
+    }
+
+    @Override
+    public void customize(Map<String, Object> hibernateProperties) {
+        hibernateProperties.put(AvailableSettings.MULTI_TENANT_IDENTIFIER_RESOLVER, this);
+    }
+
+    @Override
+    public void releaseConnection(String tenantIdentifier, Connection connection) throws SQLException {
+        connection.setSchema(BaseConstant.clientId);
+        super.releaseConnection(tenantIdentifier, connection);
     }
 }
--- src/main/java/com/xuanluan/mc/sdk/service/tenant/TenantIdentifierResolver.java
+++ src/main/java/com/xuanluan/mc/sdk/service/tenant/TenantIdentifierResolver.java
@@ -2,9 +2,13 @@ package com.xuanluan.mc.sdk.service.tenant;
 
 import com.xuanluan.mc.sdk.service.constant.BaseConstant;
 import com.xuanluan.mc.sdk.utils.StringUtils;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 
-public class TenantIdentifierResolver implements CurrentTenantIdentifierResolver {
+import java.util.Map;
+
+public class TenantIdentifierResolver implements CurrentTenantIdentifierResolver, HibernatePropertiesCustomizer {
     @Override
     public String resolveCurrentTenantIdentifier() {
         return StringUtils.hasText(TenantContext.getCurrentTenant()) ? TenantContext.getCurrentTenant() : BaseConstant.clientId;
@@ -13,5 +17,10 @@ public class TenantIdentifierResolver implements CurrentTenantIdentifierResolver
     @Override
     public boolean validateExistingCurrentSessions() {
         return true;
+    }
+
+    @Override
+    public void customize(Map<String, Object> hibernateProperties) {
+        hibernateProperties.put(AvailableSettings.MULTI_TENANT_IDENTIFIER_RESOLVER, this);
     }
 }
