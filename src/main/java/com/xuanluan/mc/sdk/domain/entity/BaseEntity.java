58d5e9ef5e462f889a04eb70b37acba948697ee8
xluan1 <shortshort1m6@gmail.com>
2024-01-08 00:30:10 +0700

    detach pre BaseEntity to BaseEntityListener

--- src/main/java/com/xuanluan/mc/sdk/domain/entity/BaseEntity.java
+++ src/main/java/com/xuanluan/mc/sdk/domain/entity/BaseEntity.java
@@ -1,8 +1,6 @@
 package com.xuanluan.mc.sdk.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.xuanluan.mc.sdk.service.constant.BaseConstant;
-import com.xuanluan.mc.sdk.utils.StringUtils;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +10,7 @@ import java.util.Date;
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(value = BaseEntityListener.class)
 public class BaseEntity {
     @Id
     private String id;
@@ -24,17 +23,4 @@ public class BaseEntity {
     @JsonFormat(shape = JsonFormat.Shape.NUMBER)
     private Date updatedAt;
     private boolean isActive = true;
-
-    @PrePersist
-    private void prePersist() {
-        if (id == null) id = StringUtils.generateId();
-        if (createdAt == null) createdAt = new Date();
-        if (createdBy == null) createdBy = BaseConstant.byUser;
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        if (updatedAt == null) updatedAt = new Date();
-        if (updatedBy == null) updatedBy = BaseConstant.byUser;
-    }
 }
---
+++ src/main/java/com/xuanluan/mc/sdk/domain/entity/BaseEntityListener.java
@@ -0,0 +1,23 @@
+package com.xuanluan.mc.sdk.domain.entity;
+
+import com.xuanluan.mc.sdk.service.constant.BaseConstant;
+import com.xuanluan.mc.sdk.utils.StringUtils;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.util.Date;
+
+public class BaseEntityListener {
+    @PrePersist
+    public void prePersist(BaseEntity entity) {
+        if (entity.getId() == null) entity.setId(StringUtils.generateId());
+        if (entity.getCreatedAt() == null) entity.setCreatedAt(new Date());
+        if (entity.getCreatedBy() == null) entity.setCreatedBy(BaseConstant.byUser);
+    }
+
+    @PreUpdate
+    public void preUpdate(BaseEntity entity) {
+        if (entity.getUpdatedAt() == null) entity.setUpdatedAt(new Date());
+        if (entity.getUpdatedBy() == null) entity.setUpdatedBy(BaseConstant.byUser);
+    }
+}
