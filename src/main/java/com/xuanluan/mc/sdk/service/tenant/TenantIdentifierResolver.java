16048360454173675aac7476425d13e2d0fb09da
xluan1 <shortshort1m6@gmail.com>
2024-01-14 23:27:32 +0700

    method switch and rollback tenant

--- src/main/java/com/xuanluan/mc/sdk/service/tenant/TenantIdentifierResolver.java
+++ src/main/java/com/xuanluan/mc/sdk/service/tenant/TenantIdentifierResolver.java
@@ -8,10 +8,26 @@ import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class TenantIdentifierResolver implements CurrentTenantIdentifierResolver, HibernatePropertiesCustomizer {
     @Setter
     private String currentTenant = BaseConstant.clientId;
+
+    /**
+     * switch to param tenant and process => after processed then rollback oldTenant
+     *
+     * @param tenant will switch
+     */
+    public <T> T switchInProcess(String tenant, Supplier<T> supplier) {
+        String oldTenant = currentTenant;
+        try {
+            currentTenant = tenant;
+            return supplier.get();
+        } finally {
+            currentTenant = oldTenant;
+        }
+    }
 
     @Override
     public String resolveCurrentTenantIdentifier() {
