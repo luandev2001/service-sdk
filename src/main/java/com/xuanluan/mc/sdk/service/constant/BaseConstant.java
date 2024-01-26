28f93f38bef300902950a4c87c0ba2266d79598a
xluan1 <shortshort1m6@gmail.com>
2024-01-26 15:43:04 +0700

    add value constant

--- src/main/java/com/xuanluan/mc/sdk/service/constant/BaseConstant.java
+++ src/main/java/com/xuanluan/mc/sdk/service/constant/BaseConstant.java
@@ -12,4 +12,10 @@ public class BaseConstant {
     public static class CacheName {
         public static final String configuration = "configurations";
     }
+
+    public static final String EVERYONE = "everyone/";
+    public static final String ADMIN = "admin/";
+    public static final String SUPER_ADMIN = "super_admin/";
+    public static final String HOOK = "hook/";
+    public static final String EMPLOYEE = "hook/";
 }
