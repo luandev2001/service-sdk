43aed829b752c634cfb9b50b5aac590442df6f23
xluan1 <shortshort1m6@gmail.com>
2024-01-26 15:43:04 +0700

    add value constant

--- src/main/java/com/xuanluan/mc/sdk/service/constant/BaseConstant.java
+++ src/main/java/com/xuanluan/mc/sdk/service/constant/BaseConstant.java
@@ -1,21 +1,16 @@
 package com.xuanluan.mc.sdk.service.constant;
 
-import org.modelmapper.Condition;
-
-import java.util.Set;
-
 public class BaseConstant {
     public static final String clientId = "public";
     public static final String orgId = "public";
     public static final String byUser = "system";
-
-    public static class CacheName {
-        public static final String configuration = "configurations";
-    }
-
     public static final String EVERYONE = "everyone/";
     public static final String ADMIN = "admin/";
     public static final String SUPER_ADMIN = "super_admin/";
     public static final String HOOK = "hook/";
-    public static final String EMPLOYEE = "hook/";
+    public static final String EMPLOYEE = "employee/";
+
+    public static class CacheName {
+        public static final String configuration = "configurations";
+    }
 }
