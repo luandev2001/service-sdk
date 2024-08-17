5877c0a19d3694d1adc4d83c00e430e4909b3850
xluan <ngoxuanluan2001@gmail.com>
2024-08-17 14:05:38 +0700

    add annotation NoRepositoryBean

--- src/main/java/com/xuanluan/mc/sdk/repository/JpaMultipleRepository.java
+++ src/main/java/com/xuanluan/mc/sdk/repository/JpaMultipleRepository.java
@@ -2,7 +2,9 @@ package com.xuanluan.mc.sdk.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface JpaMultipleRepository<T, ID>
         extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
 }
