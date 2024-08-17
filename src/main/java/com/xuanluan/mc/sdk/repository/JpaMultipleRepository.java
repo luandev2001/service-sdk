5ac5bb96a58e9c17c5638f44c9c4d393773d8acd
xluan <ngoxuanluan2001@gmail.com>
2024-08-17 13:53:06 +0700

    disable queryDsl

--- src/main/java/com/xuanluan/mc/sdk/repository/JpaMultipleRepository.java
+++ src/main/java/com/xuanluan/mc/sdk/repository/JpaMultipleRepository.java
@@ -2,8 +2,7 @@ package com.xuanluan.mc.sdk.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface JpaMultipleRepository<T, ID>
-        extends JpaRepository<T, ID>, JpaSpecificationExecutor<T>, QuerydslPredicateExecutor<T> {
+        extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
 }
